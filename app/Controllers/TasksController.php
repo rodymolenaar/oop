eb1c164e0b4b650fb3c473c5019583feae0b37a5
Rody Molenaar <rody.molenaar@xtrada.nl>
2017-10-23 13:11:17 +0200

    Add TasksController

---
+++ app/Controllers/TasksController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        return $this->view('index');
+    }
+
+    public function show()
+    {
+        return $this->view('index');
+    }
+
+    public function edit()
+    {
+        return $this->view('index');
+    }
+
+    public function update()
+    {
+        return $this->view('index');
+    }
+
+    public function create()
+    {
+        return $this->view('index');
+    }
+
+    public function store()
+    {
+        return $this->view('index');
+    }
+
+    public function delete()
+    {
+        return $this->view('index');
+    }
+}
